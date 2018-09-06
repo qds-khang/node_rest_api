8eb7bc356e8ddad80895fc250040bd76cb114c9b
Unknown <khangzhang0503@gmail.com>
2018-09-06 10:10:23 +0700

    remove unnecessary env vars

--- core/services/env.js
+++ core/services/env.js
@@ -5,11 +5,11 @@ module.exports = {
     APP_URL: process.env.APP_URL,
     MONGO_URL: process.env.MONGO_URL,
     SITE_SECRET_KEY: process.env.SITE_SECRET_KEY,
-    EMAIL: {
-        EMAIL_HOST: process.env.EMAIL_HOST,
-        EMAIL_PORT: process.env.EMAIL_PORT,
-        EMAIL_SECURE: process.env.EMAIL_SECURE,
-        EMAIL_AUTH_USER: process.env.EMAIL_AUTH_USER,
-        EMAIL_AUTH_PASS: process.env.EMAIL_AUTH_PASS
-    }
+    // EMAIL: {
+    //     EMAIL_HOST: process.env.EMAIL_HOST,
+    //     EMAIL_PORT: process.env.EMAIL_PORT,
+    //     EMAIL_SECURE: process.env.EMAIL_SECURE,
+    //     EMAIL_AUTH_USER: process.env.EMAIL_AUTH_USER,
+    //     EMAIL_AUTH_PASS: process.env.EMAIL_AUTH_PASS
+    // }
 };
